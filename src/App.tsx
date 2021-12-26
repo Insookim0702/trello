d1446c6193719d7ef36fb9c7f17689960003b772
insookim0702 <insookim0702@gmail.com>
2021-12-26 19:41:33 +0900

    - hour 및 minute input 추가 - recoil atom 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
+import React, { useState } from 'react'
+import { createGlobalStyle } from 'styled-components'
+import { useRecoilState } from 'recoil'
+import { AtomMinute } from './recoil'
+const GlobalStyle = createGlobalStyle`
+html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, 
+p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, 
+ins, kbd, q, s, samp, small, 
+strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, 
+li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, 
+tr, th, td, article, aside, canvas, details, embed, figure, figcaption, 
+footer, header, hgroup, menu, nav, 
+output, ruby, section, summary, 
+time, mark, audio, 
+video { margin: 0; padding: 0; border: 0; font-size: 100%; font: inherit; vertical-align: baseline; } /* HTML5 display-role reset for older browsers */ article, aside, details, figcaption, figure, footer, 
+header, hgroup, menu, nav, 
+section { display: block; } 
+body { line-height: 1; } 
+ol, ul { list-style: none; } 
+blockquote, q { quotes: none; } 
+blockquote:before, blockquote:after, 
+q:before, q:after { content: ''; content: none; } 
+table { border-collapse: collapse; border-spacing: 0; }
+`
+
 function App () {
-  return <div className='App'></div>
+  const [hour, setHour] = useState(0)
+  const [minute, setMinute] = useRecoilState(AtomMinute)
+  function onChange (evt: React.FormEvent<HTMLInputElement>) {
+    setMinute(+evt.currentTarget.value)
+    setHour(11)
+  }
+  return (
+    <>
+      <GlobalStyle />
+      <div>
+        <form>
+          <input type='number' onChange={onChange} />
+          <input type='number' value={hour} />
+        </form>
+      </div>
+    </>
+  )
 }
 
 export default App
---
+++ src/recoil.ts
@@ -0,0 +1,6 @@
+import { atom } from 'recoil'
+
+export const AtomMinute = atom({
+  key: 'minute',
+  default: 0
+})
